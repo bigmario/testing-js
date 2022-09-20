49d49df4b65dbdd4f25fa6d93d502268ddad9085
Mario Castro <mariocastro.pva@gmail.com>
2022-09-20 10:48:40 -0400

    Mocks

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -1,9 +1,26 @@
 const BookService = require('./books.service');
 
+const fakeBooks = [
+  {
+    _id: 1,
+    name: 'Harry Potter',
+  },
+];
+
+const spyGetAll = jest.fn();
+
+const MongoLibStub = {
+  getAll: () => [...fakeBooks],
+  create: () => { },
+};
+
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub));
+
 describe('Test for BookService', () => {
   let service;
   beforeEach(() => {
     service = new BookService();
+    jest.clearAllMocks();
   });
   describe('test for getBooks', () => {
     test('Should return book list', async () => {
@@ -13,6 +30,15 @@ describe('Test for BookService', () => {
       console.log(books);
       // Assert
       expect(books.length).toEqual(1);
+    });
+
+    test('Should return book list', async () => {
+      // Arrange
+      // Act
+      const books = await service.getBooks({});
+      console.log(books);
+      // Assert
+      expect(books[0].name).toEqual('Harry Potter');
     });
   });
 });
