acc3d60e9a263a134068e4085e30679c2209577d
Mario Castro <mariocastro.pva@gmail.com>
2022-09-28 13:04:04 -0400

    e2e

--- api/e2e/books.e2e.js
+++ api/e2e/books.e2e.js
@@ -1,31 +1,48 @@
-const mockGetAll = jest.fn();
 const request = require('supertest');
+const { MongoClient } = require('mongodb');
 const createApp = require('../src/app');
-const { generateManyBooks } = require('../src/fakes/book.fake');
+const { config } = require('../src/config');
 
-jest.mock('../src/lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
-  getAll: mockGetAll,
-  create: () => { },
-})));
+const DB_NAME = config.dbName;
+const DB_URL = config.dbUrl;
 
 describe('Test for books', () => {
   let app = null;
   let server = null;
+  let database = null;
 
-  beforeAll(() => {
+  beforeAll(async () => {
     app = createApp();
     server = app.listen(3001);
+    const client = new MongoClient(DB_URL, {
+      usenewUrlparser: true,
+      useUnifiedTopology: true,
+    });
+    await client.connect();
+    database = client.db(DB_NAME);
   });
 
   afterAll(async () => {
     await server.close();
+    await database.dropDatabase();
   });
 
   describe('Test for [GET] /api/v1/books', () => {
-    test('should return book list', () => {
+    test('should return book list', async () => {
       // Arrange
-      const fakeBooks = generateManyBooks(3);
-      mockGetAll.mockResolvedValue(fakeBooks);
+      const seedData = await database.collection('books').insertMany([
+        {
+          name: 'Book1',
+          year: 1998,
+          author: 'Mario',
+        },
+        {
+          name: 'Book2',
+          year: 1999,
+          author: 'Andres',
+        },
+      ]);
+      console.log(seedData.insertedCount);
       // Act
       return request(app)
         .get('/api/v1/books')
@@ -33,7 +50,7 @@ describe('Test for books', () => {
         .then(({ body }) => {
           console.log(body);
           // Assert
-          expect(body.length).toEqual(fakeBooks.length);
+          expect(body.length).toEqual(seedData.insertedCount);
         });
     });
   });
